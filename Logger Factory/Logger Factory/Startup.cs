08201dc9c0822f6d3a4b96013b014cf6b0eddd36
MaxIvanov44 <Lvanovmaksim200033@gmail.com>
2020-01-28 13:54:46 +0300

    lastfabric

--- Logger Factory/Logger Factory/Startup.cs
+++ Logger Factory/Logger Factory/Startup.cs
@@ -7,33 +7,24 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Logger_Factory
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void Configure(IApplicationBuilder app)
         {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+            var loggerFactory = LoggerFactory.Create(builder =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+                builder.AddDebug();
+            });
+            ILogger logger = loggerFactory.CreateLogger<Startup>();
+            app.Run(async (context) =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                logger.LogInformation("Requested Path: {0}", context.Request.Path);
+
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
